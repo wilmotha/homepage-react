361a9ec427be1952a4db30258b3d9fef1885f8f8
Austin Wilmoth <wilmotha@oregonstate.edu>
2020-02-24 22:13:24 -0800

    Added mixed link in projects

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -9,15 +9,51 @@ function Projects(props) {
         width: auto;
         margin-left: auto;
         margin-right: auto;
+        ul {
+            padding-left: 0;
+            list-style: none;
+        }
+        li {
+            padding-bottom: 10px;
+            display: inline;
+        }
+        p {
+            display: inline;
+            padding-left: 5px;
+        }
     `;
 
     return (
         <div css={styles}>
             <h1> - Projects - </h1>
             <h2> Programing Projects </h2>
-                <a href="https://github.com/wilmotha">GitHub</a>
+                <ul>
+                    <li>
+                        <a href="https://mixed-spotify.herokuapp.com/">Mixed</a>
+                        <p>
+                            - A react app that uses Spotify APIs to mix two playlists
+                        </p>
+                    </li>
+                    <br/>
+                    <br/>
+                    <li>
+                        <a href="https://github.com/wilmotha">GitHub</a>
+                        <p>
+                            - Other programing projects
+                        </p>
+                    </li>
+                    <br/>
+                </ul>
             <h2> Music Projects </h2>
-                <a href="https://soundcloud.com/austin-wilmoth">SoundCloud</a>
+                <ul>
+                    <li>
+                        <a href="https://soundcloud.com/austin-wilmoth">SoundCloud</a>
+                        <p>
+                            - Some music I made in my free time
+                        </p>
+                    </li>
+
+                </ul>
         </div>
     );
 }
